b66946df21b2c624d758e4c72e0b1e9584ecd634
O'Schell <the.gabriel.schell@gmail.com>
2020-04-12 15:49:39 +0200

    Update timestamp of <serveur_infos>>

--- index.js
+++ index.js
@@ -120,7 +120,41 @@ the command <>
     };
 
     if (command === "serveur_infos") {
-        message.channel.send(` ${message.guild.name}: ${message.guild.memberCount} total members\rChannel Count: ${message.guild.channelsCount}\rServer Region: ${message.guild.region}\rOwner: ${message.guild.owner}\rCreated: ${message.guild.createdAt}\rServer Icon: ${message.guild.icon}`);
+      // unix timestamp
+      var ts = message.guild.createdTimestamp;
+
+      // convert unix timestamp to milliseconds
+      var ts_ms = ts * 1000;
+
+      // initialize new Date object
+      var date_ob = new Date(ts_ms);
+
+      // year as 4 digits (YYYY)
+      var year = date_ob.getFullYear();
+
+      // month as 2 digits (MM)
+      var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+      // date as 2 digits (DD)
+      var date = ("0" + date_ob.getDate()).slice(-2);
+
+      // hours as 2 digits (hh)
+      var hours = ("0" + date_ob.getHours()).slice(-2);
+
+      // minutes as 2 digits (mm)
+      var minutes = ("0" + date_ob.getMinutes()).slice(-2);
+
+      // seconds as 2 digits (ss)
+      var seconds = ("0" + date_ob.getSeconds()).slice(-2);
+
+      var dte = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds;
+
+      console.log(ts.toLocaleString());
+
+      console.log("Date as YYYY-MM-DD hh:mm:ss Format: " + year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+
+      console.log(ts)
+      message.channel.send(` ${message.guild.name}: ${message.guild.memberCount} total members\rChannel Count: ${message.guild.channelsCount}\rServer Region: ${message.guild.region}\rOwner: ${message.guild.owner}\rCreated: ${dte}\rServer Icon: ${message.guild.iconURL("jpg", true, 2048)}`);
     };
 
     if (command === "token") {
