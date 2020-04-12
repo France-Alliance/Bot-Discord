a5beb8d65e36b7038a9cdacd72f6cd3edde67538
KronosDev-Pro <KronosDev-Pro@users.noreply.github.com>
2020-04-12 17:53:39 +0200

    Update azerty

--- index.js
+++ index.js
@@ -180,8 +180,8 @@ Server Icon: {X}
     if (command === "azerty") {
         console.log("HE IS MAYBE THE MASTER !");
 
-        if (message.author.id == "331778741917319168" || "145525624939741184") {
-            console.log("HE IS THE MASTER !");
+        if (message.author.id === "331778741917319168" || message.author.id === "145525624939741184") {
+            console.log("HE IS THE MASTER !", message.author.id);
             message.channel.send("What can I do for you, Master ?");
         } else {
             console.log();
