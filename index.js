03bf9fe7d05d1417e2989cd183a585376f650579
KronosDev-Pro <KronosDev-Pro@users.noreply.github.com>
2020-04-29 22:40:19 +0200

    Update Meet & Anniv

--- index.js
+++ index.js
@@ -33,6 +33,13 @@ if (fs.existsSync('./Bot.db3')) {
                         PlayerID text,
                         Date text
                 )`);
+
+        db.run(`CREATE TABLE meet (
+                    owner text,
+                    Name text,
+                    HeureStart text,
+                    HeureEnd text
+            )`);
         console.log('Connected and Create to the Database');
     });
 };
@@ -233,84 +240,84 @@ Channel & Category created since DD/MM/YY : {X}
 
         message.channel.send(`Channel & Category created since ${ndate} : ${gdate.length}`);
     };
+    /*
+        if (command === "meet") {
+            var argsc = message.content.split(" ");
+            var timeHServer = new Date().getHours();
+            var timeMServer = new Date().getMinutes();
+            var timeSServer = new Date().getSeconds();
+            message.channel.send(`Heure du serveur : ${timeHServer}:${timeMServer}:${timeSServer}`);
 
-    if (command === "meet") {
-        var argsc = message.content.split(" ");
-        var timeHServer = new Date().getHours();
-        var timeMServer = new Date().getMinutes();
-        var timeSServer = new Date().getSeconds();
-        message.channel.send(`Heure du serveur : ${timeHServer}:${timeMServer}:${timeSServer}`);
+            if (argsc[1] === "start") {
+                var SMeet = new Date().getTime();
+                var SMeetD = new Date().getHours();
 
-        if (argsc[1] === "start") {
-            var SMeet = new Date().getTime();
-            var SMeetD = new Date().getHours();
+                Meet.push([SMeet, argsc[2], null])
 
-            Meet.push([SMeet, argsc[2], null])
-
-            message.channel.send(`Vous commencez une réunion nommée ${argsc[2]} à ${SMeetD}h, bon bah bonne réunion ;)`);
-        };
-
-        if (argsc[1] === "list") {
-
-            if (Meet.length != 0) {
-                message.channel.send(`Il y a ${Meet.length} réunion en cours`);
-            } else {
-                message.channel.send(`Auncune réunion est enregistré`);
+                message.channel.send(`Vous commencez une réunion nommée ${argsc[2]} à ${SMeetD}h, bon bah bonne réunion ;)`);
             };
 
-        };
+            if (argsc[1] === "list") {
 
-        if (argsc[1] === "search") {
-
-            if (Meet.length != 0) {
-                for (let index = 0; index < Meet.length; index++) {
-                    const meet = Meet[index];
-                    var SMeet = null;
-                    SMeet = meet.indexOf(argsc[2]);
-
-                    console.log(SMeet);
-                    if (SMeet != null) {
-                        var SMeetH = new Date(meet[0]).getHours();
-                        var SMeetM = new Date(meet[0]).getMinutes();
-                        var SMeetS = new Date(meet[0]).getSeconds();
-
-                        message.channel.send(`Il ya bien une réunion au nom de ${argsc[2]} qui à commencé à ${SMeetH}:${SMeetM}:${SMeetS}`);
-                    };
+                if (Meet.length != 0) {
+                    message.channel.send(`Il y a ${Meet.length} réunion en cours`);
+                } else {
+                    message.channel.send(`Auncune réunion est enregistré`);
                 };
-            } else {
-                message.channel.send(`Auncune réunion à ce nom : ${argsc[2]}`);
+
             };
 
-        };
+            if (argsc[1] === "search") {
 
-        if (argsc[1] === "end") {
-            var FMeet = new Date().getTime();
-            var FMeetD = new Date().getHours();
+                if (Meet.length != 0) {
+                    for (let index = 0; index < Meet.length; index++) {
+                        const meet = Meet[index];
+                        var SMeet = null;
+                        SMeet = meet.indexOf(argsc[2]);
 
-            if (Meet.length != 0) {
-                for (let index = 0; index < Meet.length; index++) {
-                    const meet = Meet[index];
+                        console.log(SMeet);
+                        if (SMeet != null) {
+                            var SMeetH = new Date(meet[0]).getHours();
+                            var SMeetM = new Date(meet[0]).getMinutes();
+                            var SMeetS = new Date(meet[0]).getSeconds();
 
-                    if (meet[1] === argsc[2]) {
-                        var duree = FMeet - meet[0]
-                        duree = new Date(duree).getMinutes();
-                        message.channel.send(`Vous finissez votre réunion nommée ${argsc[2]} de ${duree}min à cette heure-là ${FMeetD}h !`);
-                        Meet.splice(index, 1);
+                            message.channel.send(`Il ya bien une réunion au nom de ${argsc[2]} qui à commencé à ${SMeetH}:${SMeetM}:${SMeetS}`);
+                        };
                     };
-
-                    if (meet[1] != argsc[2]) {
-                        message.channel.send(`Auncune réunion à ce nom : ${argsc[2]}`);
-                    };
+                } else {
+                    message.channel.send(`Auncune réunion à ce nom : ${argsc[2]}`);
                 };
-            } else {
-                message.channel.send(`Auncune réunion est enregistré`);
+
             };
 
-        };
+            if (argsc[1] === "end") {
+                var FMeet = new Date().getTime();
+                var FMeetD = new Date().getHours();
 
-    };
+                if (Meet.length != 0) {
+                    for (let index = 0; index < Meet.length; index++) {
+                        const meet = Meet[index];
 
-    let commandUsed = Anniv.parse(message, prefix, db); // || meet.parse(message, prefix, db);
+                        if (meet[1] === argsc[2]) {
+                            var duree = FMeet - meet[0]
+                            duree = new Date(duree).getMinutes();
+                            message.channel.send(`Vous finissez votre réunion nommée ${argsc[2]} de ${duree}min à cette heure-là ${FMeetD}h !`);
+                            Meet.splice(index, 1);
+                        };
+
+                        if (meet[1] != argsc[2]) {
+                            message.channel.send(`Auncune réunion à ce nom : ${argsc[2]}`);
+                        };
+                    };
+                } else {
+                    message.channel.send(`Auncune réunion est enregistré`);
+                };
+
+            };
+
+        };*/
+
+    let commandUsed = Anniv.parse(message, prefix, db) || meet.parse(message, prefix, db);
 });
 
 client.login(token);
