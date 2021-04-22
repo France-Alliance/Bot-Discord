027057c129a3562febe6feae7eba19d90cf4dc39
O'Schell <the.gabriel.schell@gmail.com>
2021-04-22 22:13:20 +0200

    Bot help message upgrade

--- commands/help.js
+++ commands/help.js
@@ -12,8 +12,9 @@ module.exports = {
 
 		if (!args.length) {
 			data.push('Here\'s a list of all my commands:');
-			data.push(commands.map(command => command.name).join(', '));
+			data.push(commands.map(command => `**${command.name}**`).join(' , '));
 			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+			data.push(`\n\nThis bot was coded by <@331778741917319168> and <@145525624939741184> with the support of France Alliance community !`);
 
 			return message.author.send(data, { split: true })
 				.then(() => {
