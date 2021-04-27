716eb74d83b7ba4889aa1a5d8d23c1897455289e
KronosDev-Pro <kronosdev.pro@gmail.com>
2021-04-28 00:15:54 +0200

    Add Plane

--- NewScrapper/Plane.py
+++ NewScrapper/Plane.py
@@ -3,6 +3,7 @@ import json
 import dotenv
 import platform
 import os
+from progress.bar import IncrementalBar as Bar
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
@@ -13,7 +14,7 @@ options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-gpu")
-options.add_argument("--log-level=3")
+options.add_argument("--log-level=1")
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -44,9 +45,10 @@ with webdriver.Chrome(executable_path=path, options=options) as driver:
     wait = WebDriverWait(driver, 10)
     connect()
     driver.get(URL_AIRCRAFT)
-    
+
     tabs = ['short', 'middle', 'long', 'cargo']
-    
+    AllResult = {'short': [], 'middle': [], 'long': [], 'cargo': []}
+
     plane = {
         "Nom": None,
         "Constructeur": None,
@@ -59,13 +61,65 @@ with webdriver.Chrome(executable_path=path, options=options) as driver:
         "Vitesse": None,
         "Prix": None
     }
-    
+    print()
+
     for t in tabs:
         driver.get(f"{URL_AIRCRAFT}{t}")
-        print("big" in driver.find_element_by_xpath('//*[@id="aircraft_buyNew_step2"]/div[1]/div[1]/div/img[1]').get_attribute('src'), wait.until(presence_of_element_located((By.XPATH, '//*[@id="aircraft_buyNew_step2"]/div[1]/div[1]/div/img[1]'))))
-        if (wait.until(presence_of_element_located((By.XPATH, '//*[@id="aircraft_buyNew_step2"]/div[1]/div[1]/div/img[1]'))) and "big" in driver.find_element_by_xpath('//*[@id="aircraft_buyNew_step2"]/div[1]/div[1]/div/img[1]').get_attribute('src')):
-            for elem in driver.find_elements_by_class_name('.aircraftPurchaseBox'):
-                print(elem)
+        result = []
+        
+        if (wait.until(presence_of_element_located((By.CLASS_NAME, 'aircraftPurchaseBox')))):
+            bar = Bar(f'{t} : ', max=(len(driver.find_elements_by_class_name('aircraftPurchaseBox'))), suffix='%(percent).1f%% (%(index)d/%(max)d) - [%(elapsed_td)s / %(eta_td)s]')
+            for i in range(1, len(driver.find_elements_by_class_name('aircraftPurchaseBox'))+1):
+                resPlane = plane.copy()
+                # Title
+                title = str(driver.find_element_by_xpath(
+                    f'//*[@id="aircraft_buyNew_step2"]/div[1]/div[2]/div[5]/div[{i}]/div[2]/span[1]').text.replace("\n", ""))
+                if "Cargo" in title:
+                    title = str(driver.find_element_by_xpath(
+                        f'//*[@id="aircraft_buyNew_step2"]/div[1]/div[2]/div[5]/div[{i}]/div[2]/span[2]').text.replace('\n', ''))
 
-    with open(f"./data/{date}.json", "w", encoding='utf8') as f:
+                # Name
+                resPlane['Nom'] = title.split('/')[0].replace(' ', '')
+
+                # Constructeur
+                resPlane['Constructeur'] = title.split('/')[1].replace(' ', '')
+
+                # Mise en service
+                resPlane['Mise en service'] = int(driver.find_element_by_xpath(
+                    f'//*[@id="aircraft_buyNew_step2"]/div[1]/div[2]/div[5]/div[{i}]/form/div[1]/div[2]/ul[1]/li[1]/b').text.replace(' ', ''))
+
+                # Rayon d'action
+                resPlane["Rayon d'action"] = int(driver.find_element_by_xpath(
+                    f'//*[@id="aircraft_buyNew_step2"]/div[1]/div[2]/div[5]/div[{i}]/form/div[1]/div[2]/ul[1]/li[2]/b').text.replace(' ', '').replace('km', ''))
+
+                # Conso
+                resPlane["Conso"] = float(driver.find_element_by_xpath(
+                    f'//*[@id="aircraft_buyNew_step2"]/div[1]/div[2]/div[5]/div[{i}]/form/div[1]/div[2]/ul[1]/li[3]/b').text.replace('L/100km/pax', '').replace(' ', ''))
+
+                # Usure
+                resPlane["Usure"] = float(driver.find_element_by_xpath(
+                    f'//*[@id="aircraft_buyNew_step2"]/div[1]/div[2]/div[5]/div[{i}]/form/div[1]/div[2]/ul[1]/li[4]/b').text.replace('%/100h', '').replace(' ', ''))
+
+                # Siege
+                resPlane["Siege"] = int(driver.find_element_by_xpath(
+                    f'//*[@id="aircraft_buyNew_step2"]/div[1]/div[2]/div[5]/div[{i}]/form/div[1]/div[2]/ul[2]/li[1]/b').text.replace(' ', ''))
+
+                # Tonnage
+                resPlane["Tonnage"] = float(driver.find_element_by_xpath(
+                    f'//*[@id="aircraft_buyNew_step2"]/div[1]/div[2]/div[5]/div[{i}]/form/div[1]/div[2]/ul[2]/li[2]/b').text.replace(' ', ''))
+
+                # Vitesse
+                resPlane["Vitesse"] = int(driver.find_element_by_xpath(
+                    f'//*[@id="aircraft_buyNew_step2"]/div[1]/div[2]/div[5]/div[{i}]/form/div[1]/div[2]/ul[2]/li[3]/b').text.replace('km/h', '').replace(' ', ''))
+
+                # Prix
+                resPlane["Prix"] = int(driver.find_element_by_xpath(
+                    f'//*[@id="aircraft_buyNew_step2"]/div[1]/div[2]/div[5]/div[{i}]/form/div[1]/div[3]/span[1]/strong').text.replace(' ', '').replace('$', ''))
+                bar.next()
+                result.append(resPlane)
+                AllResult[t] = result
+            bar.finish()
+                
+    
+    with open(f"./Plane.json", "w", encoding='utf8') as f:
         f.write(json.dumps(AllResult))
