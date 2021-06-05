4ac68535ccffc1c2473e51fd92c12053f2533be4
O'Schell <the.gabriel.schell@gmail.com>
2021-06-05 18:53:20 +0200

    GO LM

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 # Bot-Discord
-This project is Discord bot design around the needs of an Airline Manager 2 Alliance, France Alliance.
-It is something a project where we learn about coding and it will improve over time !
-If you have any question, [join the dev's Discord server](https://www.discord.gg/w5ZZVhBRK2). We will be happy to share time with you !
+![GitHub last commit](https://img.shields.io/github/last-commit/France-Alliance/Bot-Discord?style=for-the-badge)       ![GitHub commit activity](https://img.shields.io/github/commit-activity/m/France-Alliance/Bot-Discord?style=for-the-badge)       [![Discord DevLabs](https://img.shields.io/badge/Discord-Dev%20Labs-7289da?style=for-the-badge&logo=discord&logoColor=white)](https://discord.gg/w5ZZVhBRK2)       [![Discord FAC](https://img.shields.io/badge/Discord-France&nbsp;Alliance&nbsp;©-7289da?style=for-the-badge&logo=discord&logoColor=white)](https://discord.gg/ZGWHpfm)       
+
+
+
+![OS Windows](https://img.shields.io/badge/OS-Windows-blue!?style=for-the-badge&logo=windows&logoColor=white)	![OS Windows](https://img.shields.io/badge/OS-Raspbian-blue!?style=for-the-badge&logo=raspberry-pi&logoColor=white)	![Node.js](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=Node.js&logoColor=white) ![Python](https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white)
+
+## Table of content
+* [introduction](#Introduction)
+* [installation](#Installation)
+
+
+## Introduction
+
+> 🛫 This project is Discord bot design around the needs of an Airline Manager 2 Alliance, France Alliance.<br>
+> 👨‍💻 This project is way for the dev's to learn and improve about coding so it will improve over time !<br>
+> ❓ If you have any question, [join the dev's Discord server](https://www.discord.gg/w5ZZVhBRK2). We will be happy to share time with you !<br>
+
+## Installation
+
+```sh
+git clone https://github.com/France-Alliance/Bot-Discord
+
+npm install
+
+cd ./misc
+
+pip install requirements.txt
+```
+
+
+### create "token.json"
+
+```json
+{
+	"token": "YOUR_DISCORD_API_TOKEN"
+}
+```
+
+### create ".env"
+
+```
+PY_EMAIL_ACCOUNT_1="YOUR_AM2_EMAIL"
+PY_PASSWORD_ACCOUNT_1="YOUR_AM2_PASSWORD"
+CHROMEDRIVER_PATH="C:\\ABSOLUTE\\PATH\\TO\\chromedriver.exe"
+```
+
+[![forthebadge](https://forthebadge.com/images/badges/it-works-why.svg)](https://forthebadge.com)       [![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)       [![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com)       [![forthebadge](https://forthebadge.com/images/badges/powered-by-electricity.svg)](https://forthebadge.com)      
--- bot_essentials/compagnon_scripts/last_message.js
+++ bot_essentials/compagnon_scripts/last_message.js
@@ -1,22 +1,160 @@
 const discord = require(`discord.js`);
 const path = require("path");
 const fs = require("fs");
-const os = require('os')
+const os = require("os");
 
 const { token } = require(`../token.json`);
+
 const client = new discord.Client();
 
+ts = Date.now();
+
+async function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+console.log("Start script");
+var data = '';
+
 client.on("message", async (message) => {
-    /*fs.open('<fileName>',w,  function (err, file) {
-        if (!err){ 
-            console.log('File is opened in write mode.');
-        } else {
-            console.log('File is created and is now opened in write mode.');
+    console.log("");
+    console.log("------");
+    console.log("");
+
+    m_guild = message.guild;
+    m_author = message.author.toString().replace('<@', '').replace('>', '');
+    m_date = new Date(message.createdTimestamp).toISOString();
+    //console.log(m_guild.name+" ("+m_guild.id+")")
+    console.log(message.author.username +"#"+ message.author.discriminator +" ("+ message.author.id+")")
+    console.log(`@ ${m_date}`)
+  
+    /*
+    fs.open(`./${m_guild.id}.json`, "w", function (err) {
+        if (err) {
+            console.log(err);
         }
-      });*/
-    
-      console.log(client.users.fetch('697747694017314817'));
-      
-      
+    });
+
+    await sleep(500);
+    */
+
+    if(fs.existsSync(`./lm_data/${m_guild.id}.json`)) {
+        //console.log(`${m_guild.id}.json exist`);
+    } else {
+        console.log(`${m_guild.id} don't exist`);
+        fs.writeFile(`./lm_data/${m_guild.id}.json`,'{}', function (err) {
+            if (err) throw err;
+            console.log('File is created successfully.');
+          });
+
+    }
+
+    await sleep(500)
+
+    const data = JSON.parse(fs.readFileSync(`./lm_data/${m_guild.id}.json`));
+    data[m_author] = `${m_date}`;
+    fs.writeFileSync(`./lm_data/${m_guild.id}.json`, JSON.stringify(data, null, 4));
+
+    /*
+    databases = JSON.parse(content);
+  
+    try {
+      databases.push({
+          m_author: `${m_date}`,
+      });
+      fs.writeFile(`./${m_guild.id}.json`, JSON.stringify(databases, null, 4), (err) => {
+          if (err) {
+              console.log(`Error writing file: ${err}`);
+          }
+      });
+      console.log(
+        m_author.username +
+          "#" +
+          m_author.discriminator +
+          " (" +
+          m_author.id +
+          ") : " +
+          m_guild.name +
+          " (" +
+          m_guild.id +
+          ") @ " +
+          m_date
+      );
+    } catch {
+      console.log(message);
+    }
+    */
+
+        /*
+        else if (data == "") {
+            console.log("empty")
+
+            // add a new record
+            m_data = {m_author: `${m_date}`}
+
+            // write new data back to the file
+            fs.writeFile(`./${m_guild.id}.json`, JSON.stringify(m_data, null, 4), (err) => {
+                if (err) {
+                    console.log(`Error writing file: ${err}`);
+                }
+            }); 
+        }else {
+            console.log("not empty")
+
+            // add a new record
+            data.m_author = m_date
+            console.log("databases: "+databases)
+
+            // write new data back to the file
+            
+            fs.writeFile(`./${m_guild.id}.json`, JSON.stringify(databases, null, 4), (err) => {
+                if (err) {
+                    console.log(`Error writing file: ${err}`);
+                }
+            });
+        }
+        */
 });
 client.login(token);
+
+/*
+fs.open('<fileName>',w,  function (err, file) {
+    if (!err){
+        console.log('File is opened in write mode.');
+    } else {
+        console.log('File is created and is now opened in write mode.');
+    }
+});
+*/
+
+/*
+client.users.fetch(message.author.id).then(a =>
+    //console.log(a.username + " :  " + a.lastMessageID)
+    console.log(a)
+)
+*/
+
+/*
+a = Array.from(message.guild.members.cache);
+for (i in a) {
+    u = a[i][0];
+    //console.log(a[i][0])
+    client.users.fetch(u).then((b) => {
+    u_ID = b.id;
+    u_MID = b.lastMessageID
+    message.channel.messages
+        .fetch(u_MID)
+        .then((message) => {
+            console.log
+            if (message.createdTimestamp){
+            console.log(new Date(message.createdTimestamp))
+            }
+        })
+        .catch(console.error);
+    //console.log(u_ID + " :  " + u_MID);
+    content.u_ID = u_MID;
+    });
+}
+*/
